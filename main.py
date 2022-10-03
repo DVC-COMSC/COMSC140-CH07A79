3d20212276a7e3176125adb341e13921ec5d7204
Kyuwoong Lee <kwooler@gmail.com>
2022-10-02 18:05:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 
-words = input('Enter the list: ').split()
+numbers = [     [99, 11, 66, 86, 55],
+               [44, 21, 65, 88, 24],
+               [33, 77, 32, 33, 34]]
 
-are = ['a', 'r', 'e']
-idxlst = []
+
+rnum = len(numbers)
+cnum = len(numbers[0])
+
+print ('Sum of rows: ', end=' ')
+for i in range(rnum):
+	rsum = sum(numbers[i])
+	print (rsum, end=' ')
+print()
 
 # ******************************
 # Make your Code
